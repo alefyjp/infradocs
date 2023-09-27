bca712deb683a616ad5b14a3726f11ce1125a698
alefyjp <alefyjp@gmail.com>
2023-09-27 17:54:55 -0300

    Add files via upload

---
+++ scripts-linux/insala_minio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /opt/docker/minio/data
+chmod 777 /opt/docker/minio/data
+
+mkdir -p /opt/docker/minio/certs
+chmod 777 /opt/docker/minio/certs
+
+docker compose pull minio
+
